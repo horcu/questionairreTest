007e884961aed8c917454dddd0bb45daebeebee9
horcu <timbub12>
2015-10-12 10:00:38 -0400

    Fixed name conflict issue with CloudStorageEndpoint

--- Peez/backend/src/main/java/com/horcu/apps/peez/backend/storage/CloudStorageEndpoint.java
+++ Peez/backend/src/main/java/com/horcu/apps/peez/backend/storage/CloudStorageEndpoint.java
@@ -21,9 +21,9 @@ import javax.inject.Named;
                 packagePath = ""
         )
 )
-public class cloudStorageEndpoint {
+public class CloudStorageEndpoint {
 
-    private static final Logger logger = Logger.getLogger(cloudStorageEndpoint.class.getName());
+    private static final Logger logger = Logger.getLogger(CloudStorageEndpoint.class.getName());
 
     /**
      * This method gets the <code>cloudStorage</code> object associated with the specified <code>id</code>.
