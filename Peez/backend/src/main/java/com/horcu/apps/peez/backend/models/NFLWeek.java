5ccf0fac53cc53647685ef6216c63a6511d0a6e2
hacz <timbub12>
2015-10-12 10:06:19 -0400

    added backend endpoints APIs are now stable more to be added Objectify upgraded backend stable

---
+++ Peez/backend/src/main/java/com/horcu/apps/peez/backend/models/NFLWeek.java
@@ -0,0 +1,55 @@
+package com.horcu.apps.peez.backend.models;
+
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+
+
+import java.util.Date;
+
+/**
+ * Created by hacz on 10/10/2015.
+ */
+@Entity
+public class NFLWeek {
+    @Id
+    private long id;
+
+    @Index
+    private int weekNumber;
+
+    private java.util.Date dateRangeStart;
+
+    private Date dateRangeEnd;
+
+    private String type;
+
+    private String games;
+
+    public NFLWeek() {
+
+    }
+
+
+    public int getWeekNumber() {
+        return weekNumber;
+    }
+
+    public Date getDateRangeStart() {
+        return dateRangeStart;
+    }
+
+    public Date getDateRangeEnd() {
+        return dateRangeEnd;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getGames() {
+        return games;
+    }
+}
---
+++ Peez/backend/src/main/java/com/horcu/apps/peez/backend/models/Stats.java
@@ -0,0 +1,7 @@
+package com.horcu.apps.peez.backend.models;
+
+/**
+ * Created by hacz on 10/10/2015.
+ */
+public class Stats {
+}
