d179087428fb7e1662d9828c4b4f7c7b754a8003
hcummings <horatio.cummings@gmail.com>
2016-03-01 22:42:45 -0500

    sync

---
+++ Peez/bindingcollectionadapter-recyclerview/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="3bc178a4-a5f8-4785-b5a8-5100bdfcce86", modulePackage="me.tatarka.bindingcollectionadapter.recyclerview", sdkRoot="C:\\MyData\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\MyData\\StudioProjects\\Peez\\Peez\\bindingcollectionadapter-recyclerview\\build\\intermediates\\data-binding-info\\release",exportClassListTo="C:\\MyData\\StudioProjects\\Peez\\Peez\\bindingcollectionadapter-recyclerview\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=10,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
